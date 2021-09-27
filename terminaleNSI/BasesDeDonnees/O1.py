f34c34364082dc7d7135398aad46a84282ee3528
Dendaletche <jodenda@gmail.com>
2021-09-27 20:27:01 +0200

    Create O1.py

---
+++ terminaleNSI/BasesDeDonnees/O1.py
@@ -0,0 +1,16 @@
+import O2
+
+class OBJET1 :
+    def __init__(self):
+        self._O2 = OBJET2()
+
+if __name__ == '__main__' :
+    """
+    produit une erreur de recursion
+     File "C:/Users/Violette/SitesWeb/terminaleNSI/BasesDeDonnees/objet1.py", line 7, in __init__
+    self._O1 = OBJET1()
+  File "C:/Users/Violette/SitesWeb/terminaleNSI/BasesDeDonnees/objet1.py", line 3, in __init__
+    self._O2 = OBJET2()
+RecursionError: maximum recursion depth exceeded
+    """
+    O = OBJET1()
